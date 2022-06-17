777ca2b7d6b00c4d2b7075168cc3569debef21c6
Maicon Antônio Cunico <maiconcunico@gmail.com>
2022-06-17 19:10:04 -0300

    initial commit, base complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS Voting Machine
+
+Brazilian electronic voting machine made with JavaScript, HTML and CSS
+
+Base code made together with https://youtu.be/hF_VMWnsY00
---
+++ src/js/etapas.js
@@ -0,0 +1,76 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '58337',
+                nome: 'Antônio',
+                partido: 'ABC',
+                fotos: [
+                    {url: '58337.png', legenda: 'Vereador',}
+                ]
+            },
+            {
+                numero: '94679',
+                nome: 'Antônio',
+                partido: 'DEFG',
+                fotos: [
+                    {url: '94679.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '55457',
+                nome: 'Clovis',
+                partido: 'HIJK',
+                fotos: [
+                    {url: '55457.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '39191',
+                nome: 'Quintino',
+                partido: 'XYZ',
+                fotos: [
+                    {url: '39191.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '16',
+                nome: 'Alice',
+                partido: 'ABC',
+                vice: 'Kate',
+                fotos: [
+                    {url: '16.png', legenda: 'Prefeito'},
+                    {url: '16-02.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '48',
+                nome: 'Kellvin',
+                partido: 'DEFG',
+                vice: 'Bob',
+                fotos: [
+                    {url: '48.png', legenda: 'Prefeito'},
+                    {url: '48-02.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '53',
+                nome: 'Jair',
+                partido: 'XYZ',
+                vice: 'Leila',
+                fotos: [
+                    {url: '53.png', legenda: 'Prefeito'},
+                    {url: '53-02.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    }
+]
